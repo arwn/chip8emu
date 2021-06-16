49e584301e64485e600e38a05d5e28a72a9bc0a6
Aren Windham <arwn@2603-8081-4d00-f219-124a-7dff-fe08-9c79.res6.spectrum.com>
2021-06-15 21:15:14 -0500

    basic tests pass

--- src/main.zig
+++ src/main.zig
@@ -36,70 +36,238 @@ pub fn main() anyerror!void {
     @memcpy(memory[0x50..], font[0..], 0xf * 5);
     program_counter = 0x200;
     try load();
-    try mainLoop();
+    mainLoop();
 }
 
-fn mainLoop() !void {
+fn mainLoop() void {
     var i: i32 = 0;
     while (true) {
         const byte_a = memory[program_counter];
         const byte_b = memory[program_counter + 1];
-        execute(byte_a, byte_b);
+        const instruction: u16 = (@as(u16, byte_a) << 8) | @as(u16, byte_b);
+        execute(instruction);
     }
 }
 
-fn execute(byte_a: u8, byte_b: u8) void {
-    const instruction: u16 = (@as(u16, byte_a) << 8) | @as(u16, byte_b);
+fn execute(instruction: u16) void {
+    switch (instruction & 0xf000) {
+        0x0000 => {
+            if (instruction == 0x00e0) {
+                program_counter += 2;
+            } else if (instruction == 0x00ee) {
+                stack_pointer -= 1;
+                program_counter = stack[stack_pointer];
+            } else {
+                unreachable;
+            }
+        },
 
-    if (instruction == 0x00e0) {
-        program_counter += 2;
-    } else if (instruction == 0x00ee) {
-        stack_pointer -= 1;
-        program_counter = stack[stack_pointer];
-    } else if ((instruction & 0xf000) == 0x2000) {
-        const jump_to = instruction & 0x0fff;
-        stack[stack_pointer] = program_counter;
-        stack_pointer += 1;
-        program_counter = jump_to;
-    } else if ((instruction & 0xf000) == 0xa000) {
-        index_register = instruction & 0x0fff;
-        program_counter += 2;
-    } else if ((instruction & 0xf000) == 0x6000) {
-        const r = (instruction & 0x0f00) >> 8;
-        const val: u8 = @truncate(u8, instruction & 0x00ff);
-        register[r] = val;
-        program_counter += 2;
-    } else if ((instruction & 0xf000) == 0x7000) {
-        const r = (instruction & 0x0f00) >> 8;
-        const value = instruction & 0x00ff;
-        register[r] += @truncate(u8, value);
-        program_counter += 2;
-    } else if ((instruction & 0xf000) == 0xd000) {
-        const vx: u8 = @truncate(u8, (instruction & 0x0f00) >> 8);
-        const vy: u8 = @truncate(u8, (instruction & 0x00f0) >> 4);
-        const h: u8 = @truncate(u8, (instruction & 0x000f));
-        var xpos = register[vx] % 64;
-        var ypos = register[vy] % 32;
-        var row: u8 = 0;
-        while (row < h) : (row += 1) {
-            const sprite_bype = memory[index_register + row];
-            var col: u8 = 0;
-            while (col < 8) : (col += 1) {
-                const x80: u8 = 0x80;
-                const s: u8 = x80 >> @truncate(u3, col);
-                const sprite_pixel: u8 = sprite_bype & s;
-                const screen_pixel: *u32 = &display[@as(u32, (ypos + row)) * 32 + xpos + col];
-                if (sprite_pixel != 0) {
-                    screen_pixel.* ^= 0xffffffff;
+        0x1000 => program_counter = instruction & 0x0fff,
+
+        0x2000 => {
+            const jump_to = instruction & 0x0fff;
+            stack[stack_pointer] = program_counter;
+            stack_pointer += 1;
+            program_counter = jump_to;
+        },
+
+        0x3000 => {
+            const x = (instruction & 0x0f00) >> 8;
+            const kk = instruction & 0x00ff;
+            if (register[x] == kk) {
+                program_counter += 4;
+            } else {
+                program_counter += 2;
+            }
+        },
+
+        // 4xkk - SNE Vx, byte
+        0x4000 => {
+            // Skip next instruction if Vx != kk.  The interpreter
+            // compares register Vx to kk, and if they are not
+            // equal,increments the program counter by 2.
+            const x = (instruction & 0x0f00) >> 8;
+            const kk = (instruction & 0x00ff);
+            if (register[x] != kk) {
+                program_counter += 4;
+            } else {
+                program_counter += 2;
+            }
+        },
+
+        // 5xy0 - SE Vx, Vy
+        0x5000 => {
+            // Skip next instruction if Vx = Vy.  The interpreter
+            // compares register Vx to register Vy, and if they are
+            // equal,increments the program counter by 2.
+            const x = (instruction & 0x0f00) >> 8;
+            const y = (instruction & 0x00f0) >> 4;
+            if (register[x] == register[y]) {
+                program_counter += 4;
+            } else {
+                program_counter += 2;
+            }
+        },
+
+        // 6xkk - LD Vx, byte
+        0x6000 => {
+            // Set Vx = kk.  The interpreter puts the value kk into
+            // register Vx.
+            const x = (instruction & 0x0f00) >> 8;
+            const kk = @truncate(u8, instruction & 0x00ff);
+            register[x] = kk;
+            program_counter += 2;
+        },
+
+        // 7xkk - ADD Vx, byte
+        0x7000 => {
+            // Set Vx = Vx + kk.  Adds the value kk to the value of
+            // register Vx, then stores the result in Vx.
+            const r = (instruction & 0x0f00) >> 8;
+            const value = instruction & 0x00ff;
+            register[r] += @truncate(u8, value);
+            program_counter += 2;
+        },
+
+        0x8000 => {
+            const x = (instruction & 0x0f00) >> 8;
+            const y = (instruction & 0x00f0) >> 4;
+            switch (instruction & 0x000f) {
+                // 8xy0 - LD Vx, Vy
+                0x0000 => {
+                    // Set Vx = Vy.
+                    register[x] = register[y];
+                },
+
+                // 8xy1 - OR Vx, Vy
+                0x0001 => {
+                    // Set Vx = Vx OR Vy.
+                    register[x] |= register[y];
+                },
+
+                // 8xy2 - AND Vx, Vy
+                0x0002 => {
+                    // Set Vx = Vx AND Vy.
+                    register[x] &= register[y];
+                },
+
+                // 8xy3 - XOR Vx, Vy
+                0x0003 => {
+                    // Set Vx = Vx XOR Vy.
+                    register[x] ^= register[y];
+                },
+
+                // 8xy4 - ADD Vx, Vy
+                0x0004 => {
+                    // Set Vx = Vx + Vy, set VF = carry.  If the
+                    // result is greaterthan 8 bits VF is set to 1,
+                    // otherwise 0.
+                    if (@addWithOverflow(u8, register[x], register[y], &register[x])) {
+                        register[0xf] = 1;
+                    } else {
+                        register[0xf] = 0;
+                    }
+                },
+
+                // 8xy5 - SUB Vx, Vy
+                0x0005 => {
+                    // Set Vx = Vx - Vy, set VF = NOT borrow.  If Vx <
+                    // Vy, then VF is set to 1, otherwise 0.
+                    if (@subWithOverflow(u8, register[x], register[y], &register[x])) {
+                        register[0xf] = 0;
+                    } else {
+                        register[0xf] = 1;
+                    }
+                },
+
+                // 8xy6 - SHR Vx{, Vy}
+                0x0006 => {
+                    // Set Vx = Vx SHR 1.  If the least-significant
+                    // bit of Vx is 1, then VF is set to 1, otherwise
+                    // 0.
+                    register[0xf] = register[x] & 1;
+                    register[x] >>= 1;
+                },
+
+                // 8xy7 - SUBN Vx, Vy
+                0x0007 => {
+                    // Set Vx = Vy - Vx, set VF = NOT borrow.  If Vy <
+                    // Vx, then VF is set to 1, otherwise 0.
+                    if (@subWithOverflow(u8, register[y], register[x], &register[x])) {
+                        register[0xf] = 1;
+                    } else {
+                        register[0xf] = 0;
+                    }
+                },
+
+                // 8xyE - SHL Vx{, Vy}
+                0x000e => {
+                    // Set Vx = Vx SHL 1.  If the most-significant bit
+                    // of Vx is 1, then VF is set to 1, otherwise to
+                    // 0.
+                    if (@shlWithOverflow(u8, register[x], 1, &register[x])) {
+                        register[0xf] = 1;
+                    } else {
+                        register[0xf] = 0;
+                    }
+                },
+
+                else => unreachable,
+            }
+            program_counter += 2;
+        },
+
+        // 9xy0 - SNE Vx, Vy
+        0x9000 => {
+            // Skip next instruction if Vx != Vy.
+            const x = (instruction & 0x0f00) >> 8;
+            const y = (instruction & 0x00f0) >> 4;
+            if (register[x] != register[y]) {
+                program_counter += 4;
+            } else {
+                program_counter += 2;
+            }
+        },
+
+        // Annn - LD I, addr
+        0xa000 => {
+            // Set I = nnn.
+            index_register = instruction & 0x0fff;
+            program_counter += 2;
+        },
+
+        // Bnnn - JP V0, addr
+        0xb000 => {
+            // Jump to location nnn + V0.
+            const nnn = instruction & 0x0fff;
+            program_counter = register[0] + nnn;
+        },
+
+        0xd000 => {
+            const vx: u8 = @truncate(u8, (instruction & 0x0f00) >> 8);
+            const vy: u8 = @truncate(u8, (instruction & 0x00f0) >> 4);
+            const h: u8 = @truncate(u8, (instruction & 0x000f));
+            var xpos = register[vx] % 64;
+            var ypos = register[vy] % 32;
+            var row: u8 = 0;
+            while (row < h) : (row += 1) {
+                const sprite_bype = memory[index_register + row];
+                var col: u8 = 0;
+                while (col < 8) : (col += 1) {
+                    const x80: u8 = 0x80;
+                    const s: u8 = x80 >> @truncate(u3, col);
+                    const sprite_pixel: u8 = sprite_bype & s;
+                    const screen_pixel: *u32 = &display[@as(u32, (ypos + row)) * 32 + xpos + col];
+                    if (sprite_pixel != 0) {
+                        screen_pixel.* ^= 0xffffffff;
+                    }
                 }
             }
-        }
-        refreshDisplay() catch {};
-        program_counter += 2;
-    } else if ((instruction & 0xf000) == 0x1000) {
-        program_counter = instruction & 0x0fff;
-    } else {
-        unreachable;
+            refreshDisplay() catch {};
+            program_counter += 2;
+        },
+
+        else => unreachable,
     }
 }
 
@@ -136,7 +304,7 @@ fn load() !void {
 }
 
 test "execute 00e0 cls" {
-    execute(0x00, 0xe0);
+    execute(0x00e0);
     for (display) |m| {
         try expect(m == 0);
     }
@@ -147,24 +315,24 @@ test "execute 00ee ret" {
     const old_sp = stack_pointer;
     program_counter = 1;
     const old_pc = program_counter;
-    execute(0x00, 0xee);
+    execute(0x00ee);
     try expect(stack_pointer == old_sp - 1);
     try expect(stack_pointer == old_sp - 1);
     try expect(program_counter == stack[stack_pointer]);
 }
 
 test "execute 1nnn jump" {
-    execute(0x10, 0x00);
+    execute(0x1000);
     try expect(program_counter == 0x0);
 
-    execute(0x1f, 0xff);
+    execute(0x1fff);
     try expect(program_counter == 0x0fff);
 }
 
 test "execute 2nnn call" {
     const old_pc = program_counter;
     const old_sp = stack_pointer;
-    execute(0x20, 0x0f);
+    execute(0x200f);
     try expect(program_counter == 0x00f);
     try expect(stack_pointer == old_sp + 1);
     try expect(stack[old_sp] == old_pc);
@@ -173,22 +341,22 @@ test "execute 2nnn call" {
 test "execute 3xkk skip eq" {
     const old_pc = program_counter;
     register[3] = 1;
-    execute(0x33, 0x01);
+    execute(0x3301);
     try expect(old_pc + 4 == program_counter);
 
     program_counter = old_pc;
-    execute(0x33, 0xff);
+    execute(0x33ff);
     try expect(old_pc + 2 == program_counter);
 }
 
-test "execute 4xkk skip ne" {
+test "4xkk - SNE Vx, byte" {
     const old_pc = program_counter;
     register[3] = 1;
-    execute(0x43, 0x01);
+    execute(0x4301);
     try expect(old_pc + 2 == program_counter);
 
     program_counter = old_pc;
-    execute(0x43, 0xff);
+    execute(0x43ff);
     try expect(old_pc + 4 == program_counter);
 }
 
@@ -196,33 +364,37 @@ test "execute 5xy0 skip if x = y" {
     const old_pc = program_counter;
     register[3] = 0x1;
     register[5] = 0xf;
-    execute(0x53, 0x50);
+    execute(0x5350);
     try expect(old_pc + 2 == program_counter);
 
     program_counter = old_pc;
     register[3] = 0xf;
     register[5] = 0xf;
-    execute(0x53, 0x50);
+    execute(0x5350);
     try expect(old_pc + 4 == program_counter);
 }
 
 test "execute 6xkk load vx kk" {
-    for (register) |_, i| {
-        execute(0x60 + @truncate(u8, i), 0xfa);
-        try expect(register[i] == 0xfa);
-    }
+    execute(0x64fa);
+    try expect(register[4] == 0xfa);
+
+    execute(0x6fee);
+    try expect(register[0xf] == 0xee);
+
+    execute(0x6011);
+    try expect(register[0x0] == 0x11);
 }
 
 test "execute 7xkk add vx kk" {
     register[2] = 1;
-    execute(0x72, 0x03);
+    execute(0x7203);
     try expect(register[2] == 4);
 }
 
 test "execute 8xy0 ld vx vy" {
     register[1] = 0xaa;
     register[2] = 0xff;
-    execute(0x81, 0x20);
+    execute(0x8120);
     try expect(register[1] == register[2]);
     try expect(register[1] == 0xff);
 }
@@ -230,7 +402,7 @@ test "execute 8xy0 ld vx vy" {
 test "execute 8xy1 vx or vy" {
     register[1] = 0x10;
     register[2] = 0x1f;
-    execute(0x81, 0x21);
+    execute(0x8121);
     try expect(register[1] == 0x10 | 0x1f);
     try expect(register[2] == 0x1f);
 }
@@ -238,7 +410,7 @@ test "execute 8xy1 vx or vy" {
 test "execute 8xy2 vx and vy" {
     register[1] = 0x10;
     register[2] = 0x1f;
-    execute(0x81, 0x22);
+    execute(0x8122);
     try expect(register[1] == 0x10 & 0x1f);
     try expect(register[2] == 0x1f);
 }
@@ -246,7 +418,7 @@ test "execute 8xy2 vx and vy" {
 test "execute 8xy3 vx xor vy" {
     register[1] = 0x10;
     register[2] = 0x1f;
-    execute(0x81, 0x23);
+    execute(0x8123);
     try expect(register[1] == 0x10 ^ 0x1f);
     try expect(register[2] == 0x1f);
 }
@@ -254,13 +426,13 @@ test "execute 8xy3 vx xor vy" {
 test "execute 8xy4 add vx vy" {
     register[1] = 2;
     register[2] = 3;
-    execute(0x82, 0x34);
+    execute(0x8124);
     try expect(register[1] == 5);
-    try expect(register[15] == 0);
+    try expect(register[2] == 3);
 
     register[1] = 255;
     register[2] = 1;
-    execute(0x82, 0x34);
+    execute(0x8124);
     try expect(register[1] == 0);
     try expect(register[15] == 1);
 }
@@ -268,41 +440,41 @@ test "execute 8xy4 add vx vy" {
 test "execute 8xy5 sub vx vy" {
     register[1] = 5;
     register[2] = 1;
-    execute(0x81, 0x25);
+    execute(0x8125);
     try expect(register[1] == 4);
     try expect(register[2] == 1);
     try expect(register[15] == 1);
 
     register[1] = 4;
     register[2] = 5;
-    execute(0x81, 0x25);
+    execute(0x8125);
     try expect(register[1] == 255);
-    try expect(register[2] == 2);
+    try expect(register[2] == 5);
     try expect(register[15] == 0);
 }
 
 test "execute 8xy6 shr vx" {
     register[1] = 0xfd;
-    execute(0x81, 0x06);
+    execute(0x8106);
     try expect(register[1] == 0xfd >> 1);
     try expect(register[15] == 0xfd & 1);
 
-    execute(0x81, 0x06);
+    execute(0x8106);
     try expect(register[1] == 0xfd >> 2);
     try expect(register[15] == (0xfd >> 1) & 1);
 }
 
 test "execute 8xy7 subn vx vy" {
-    register[1] = 12;
-    register[2] = 6;
-    execute(0x81, 0x27);
+    register[1] = 6;
+    register[2] = 12;
+    execute(0x8127);
     try expect(register[1] == 12 - 6);
-    try expect(register[2] == 6);
-    try expect(register[15] == 1);
+    try expect(register[2] == 12);
+    try expect(register[15] == 0);
 
     register[1] = 0;
     register[2] = 0;
-    execute(0x81, 0x27);
+    execute(0x8127);
     try expect(register[1] == 0);
     try expect(register[2] == 0);
     try expect(register[15] == 0);
@@ -310,41 +482,51 @@ test "execute 8xy7 subn vx vy" {
 
 test "execute 8xyE shl vx" {
     register[1] = 6;
-    execute(0x81, 0x0E);
+    execute(0x810E);
     try expect(register[1] == 6 << 1);
     try expect(register[15] == 0);
 
     register[1] = 0xff;
-    execute(0x81, 0x0E);
-    try expect(register[1] == 0xff << 1);
-    try expect(register[15] == 0);
+    execute(0x810E);
+    try expect(register[1] == @truncate(u8, 0xff << 1));
+    try expect(register[15] == 1);
 }
 
 test "execute skip ne vx vy" {
     const old_pc = program_counter;
     register[1] = 1;
     register[2] = 1;
-    execute(0x91, 0x20);
+    execute(0x9120);
     try expect(program_counter == old_pc + 2);
 
     register[2] = 0xff;
     register[3] = 0;
-    execute(0x92, 0x30);
+    execute(0x9230);
     try expect(program_counter == old_pc + 6);
 }
 
 test "execute annn load index_register nnn" {
-    execute(0xaf, 0xff);
+    execute(0xafff);
     try expect(index_register == 0xfff);
-    execute(0xa1, 0x23);
+    execute(0xa123);
     try expect(index_register == 0x123);
 }
 
 test "execute bnnn jump v0 nnn" {
-    const old_pc = program_counter;
-    const v0 = register[0];
-    execute(0xbf, 0x00);
-    try expect(program_counter == v0 + 0xf00);
+    var old_pc = program_counter;
+    register[0] = 0;
+    execute(0xbf00);
+    try expect(program_counter == 0x0f00);
+
+    old_pc = program_counter;
+    register[0] = 3;
+    execute(0xbf00);
+    try expect(program_counter == 0x0f03);
+
+    old_pc = program_counter;
+    register[0] = 3;
+    execute(0xbf03);
+    try expect(program_counter == 0x0f06);
 }
 
 test "execute cxkk rnd vx kk" {
